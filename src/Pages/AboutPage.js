d5be5948b818eed6303ed89fb6e4e6ce74a3f0a7
Mitraws <m.ws@outlook.com>
2020-05-05 01:23:30 +0200

    subtitle change

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -4,6 +4,6 @@ import Title from '../Components/Title';
 
 export default function () {
     // console.log("Hello from about page")
-    return <Title subtitle="Welcome my friend, you just discovered a uber cool movie app.">Hello!</Title>
+    return <Title subtitle="Welcome my friend, you just discovered the movie app of all time.">Hello!</Title>
 }
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,11 @@
 import React from "react";
-import Title from '../Components/Title';
+import Title from "../Components/Title";
 
 export default function (props) {
-    // console.log("Hello from home page")
-    return <Title subtitle= "Click on Movies for your favourite entertainment.">Hello!</Title>
+  // console.log("Hello from home page")
+  return (
+    <Title subtitle="Click on Movies and get entertained entertainment.">
+      Hello!
+    </Title>
+  );
 }
