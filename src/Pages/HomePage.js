1f9c02c4ac29dcfef8c37331c38287f74a98de39
Mitraws <m.ws@outlook.com>
2020-05-05 01:25:05 +0200

    title changes

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,7 +4,7 @@ import Title from "../Components/Title";
 export default function (props) {
   // console.log("Hello from home page")
   return (
-    <Title subtitle="Click on Movies and get entertained entertainment.">
+    <Title subtitle="Click on Movies and get entertained.">
       Hello!
     </Title>
   );
